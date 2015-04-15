bf52fd7d8d9f7247a13b779fda495522b48f0e71
chutichhuy <vnsummer@gmail.com>
2015-04-16 01:55:43 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# SVG Sprite Polyfill
+
+Icon system using SVG sprite can be archived with the method described in this article: https://css-tricks.com/svg-symbol-good-choice-icons/
+
+This polyfill's purpose is to fix a bug on Webkit-based browsers (Chrome, Safari and the like) that makes it unable to style icons with usual CSS.
+
+There's no need for this on Firefox.
+
---
+++ polyfill.js
@@ -0,0 +1,91 @@
+(function (requestAnimationFrame) {
+    // create a sprite svg element and hide it somewhere
+    var createSpriteElement = function (url, svgText) {
+        var wrap            = document.createElement('span');
+        wrap.innerHTML      = svgText;
+        wrap.style.display  = 'none';
+
+        // append
+        document.body.appendChild(wrap);
+        return;
+    };
+    
+    // load sprite svg using AJAX
+    var loadSprite = function (file, cb) {
+        var x = new (XMLHttpRequest || ActiveXObject)('MSXML2.XMLHTTP.3.0');
+        x.open('GET', file, 1);
+        // 
+        x.onreadystatechange = function () {
+            if (x.readyState > 3) {
+                createSpriteElement(file, x.responseText);
+                cb();
+            }
+            return;
+        };
+        x.send();
+        return file;
+    }; 
+    
+    var modifyElement = function (e, uri) {
+        return (function (symbol) {
+            if (!symbol) {
+                return void(0);
+            } else {
+                // e.parentNode.setAttribute('viewBox', symbol.getAttribute('viewBox'));
+                e.parentNode.replaceChild((function (nodes, frag) {
+                    Array.prototype.forEach.call(nodes, function (n) {
+                        frag.appendChild(n.cloneNode(true));
+                    });
+                    return frag;
+                })(symbol.childNodes, document.createDocumentFragment()), e);
+                return void(0);
+            }
+        })(document.getElementById(uri.substring(1)));
+    }; 
+
+    var createScanner = function (loaded) {
+        return function () {
+            (function scanEle(n, l, counter) {
+                return counter >= n.length      ?
+                    
+                    (function () {
+                        // setup next scanner
+                        requestAnimationFrame(createScanner(l));
+                        return void(0);
+                    })()                        :
+
+                    (function (href) {
+                        return (href[0] == '#') ? void(0) : (function (parts) {
+                            
+                            return (function (uri, hash) {
+                                // if the file hasn't been loaded before,
+                                // load it into DOM
+                                scanEle(n                               ,
+                                    l.indexOf(uri) > -1                 ?
+                                    (function () {
+                                        modifyElement(n[counter], hash);
+                                        return l;
+                                    })()                                :
+                                    (function () {
+                                        return l.concat([
+                                            loadSprite(uri, function () {
+                                                // now modify the e element
+                                                return modifyElement(n[counter], hash);
+                                            })
+                                        ]);
+                                    })()                                ,
+                                    (counter + 1)
+                                );
+                                return void(0);
+                            })(parts[0], parts[1]);
+
+                        })(href.split('#'));
+
+                    })(n[counter].getAttribute('xlink:href'));
+
+            })(document.getElementsByTagName('use'), loaded, 0); 
+        };
+    };
+
+    createScanner([])();
+})(window.requestAnimationFrame || window.setTimeout);
